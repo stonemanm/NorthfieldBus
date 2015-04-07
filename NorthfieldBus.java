c9cde3e325038f925fca16ea1d956459c9bf7331
Michael Stoneman <azureillusions@eduroam-171-201.dyn.carleton.edu>
2015-04-07 00:32:02 -0500

    Finished it! There's probably some cleaning up to be done, but it works beautifully with GeekTool on my desktop, and that's what matters for now.

--- NorthfieldBus.java
+++ NorthfieldBus.java
@@ -1,5 +1,6 @@
 /**
  * A simple program to display the next available bus from Carleton.
+ * 
  * @author Michael Stoneman
  * @version Last modified 2015-04-06
  */
@@ -8,24 +9,30 @@ import java.util.ArrayList;
 import java.time.LocalDate;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class NorthfieldBus {
-	// Conveniently, the default length of ArrayList is 10.
-	private ArrayList<LocalTime> busTimes = new ArrayList<LocalTime>();
-	private LocalTime t0 = null;
+	private static LocalTime timeNow = LocalTime.now();
+	// private static LocalTime timeNow = LocalTime.of(16,33);
 
-	public void dayCheck(ArrayList<LocalTime> busTimes) {
+	/**
+	 * Fills an ArrayList with LocalTime objects corresponding to the bus times
+	 * for the day.
+	 * 
+	 * @return Nothing. It alters a list.
+	 */
+	public static void dayCheck(ArrayList<LocalTime> busTimes) {
 		LocalDate curDate = LocalDate.now();
 		DayOfWeek isSunday = curDate.getDayOfWeek();
 
 		if (isSunday.getValue() == 7) { // If it's Sunday,
-			t0 = LocalTime.of(1,22); // start at 1:22...
+			LocalTime t0 = LocalTime.of(13,22); // start at 1:22...
 			busTimes.add(t0);
 			for (int i = 0; i < 3; i++) { // ...and do this three times.
 				busTimes.add(busTimes.get(i).plusMinutes(40));
 			}
 		} else {
-			t0 = LocalTime.of(4,22);
+			LocalTime t0 = LocalTime.of(16,22);
 			busTimes.add(t0);
 			for (int i = 0; i < 9; i++) {
 				busTimes.add(busTimes.get(i).plusMinutes(40));
@@ -33,14 +40,59 @@ public class NorthfieldBus {
 		}
 	}
 
-	public void printList(ArrayList<LocalTime> aList) {
-		for (int i = 0; i < aList.size(); i++) {
-			System.out.println(aList.get(i).toString());
+	/**
+	 * Computes the time until the next bus departs Carleton into town.
+	 */
+	public static int timeUntilTown(ArrayList<LocalTime> busTimes) {
+		int i = 0;
+		while (i < busTimes.size() && busTimes.get(i).compareTo(timeNow) != 1) {
+			i++;
+		} //loop breaks for smallest i that gives a 1 or after checking all.
+		if (i >= busTimes.size()) { return -1; }
+		int minsUntil = (int) timeNow.until(busTimes.get(i), MINUTES);
+		if (minsUntil > 60) {
+			return -1;
+		} else {
+			return minsUntil;
+		}
+	}
+
+	/**
+	 * Computes the time until the next bus departs Carleton for Olaf.
+	 */
+	public static int timeUntilOlaf(ArrayList<LocalTime> busTimes) {
+		int i = 0;
+		while (i < busTimes.size() && busTimes.get(i).plusMinutes(20).compareTo(timeNow) != 1) {
+			i++;
+		} //loop breaks for smallest i that gives a 1 or after checking all.
+		if (i >= busTimes.size()) { return -1; }
+		int minsUntil = (int) timeNow.until(busTimes.get(i).plusMinutes(20), MINUTES);
+		if (minsUntil > 60) {
+			return -1;
+		} else {
+			return minsUntil;
 		}
 	}
 
 	public static void main(String[] args) {
+		ArrayList<LocalTime> busTimes = new ArrayList<LocalTime>();
 		dayCheck(busTimes);
-		printList(busTimes);
+		if (args[0].equals("town")) {
+			int untilTown = timeUntilTown(busTimes);
+			if (untilTown == -1) {
+				System.out.println("--");
+			} else {
+				System.out.println(untilTown);
+			}	
+		}
+		if (args[0].equals("olaf")) {
+			int untilOlaf = timeUntilOlaf(busTimes);
+			if (untilOlaf == -1) {
+				System.out.println("(Not running to Olaf now.)");
+			} else {
+				System.out.println("(" + untilOlaf + " minutes for bus to Olaf.)");
+			}	
+		}
+		
 	}
 }
